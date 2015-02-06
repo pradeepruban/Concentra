ff12cd6e19619ba1ba70254832c423e3ce5497c9
P28001 <P28001@816f1645-bdcd-3547-a8e0-97d3e897fc65>
2015-02-06 11:51:55 +0000

    git-svn-id: https://syncgdc1971.vcloud.synlab.site/svn/ISAP_UI_Provision/ISAP_UI_DEV/branches@36 816f1645-bdcd-3547-a8e0-97d3e897fc65

---
+++ src/main/java/com/isap/core/requests/xsd/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://requests.core.isap.com/xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.isap.core.requests.xsd;
